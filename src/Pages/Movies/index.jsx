e8b552489c50f5a8d00ad66f3131c59856d87cb2
sh <sh@gmail.com>
2022-11-04 19:30:22 +0300

    added fetchMovies movie page

--- src/Pages/Movies/index.jsx
+++ src/Pages/Movies/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+
 
 const Movies = () => {
+    const [page, setPage] = useState(1)
+
+    const fetchMovies = async () => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`)
+        
+        console.log(data);
+    }
+
+    useEffect(() => {
+        fetchMovies()
+    }, [page])
+
+
     return (
         <div>
             <span className='pageTitle'>Movies</span>
+
         </div>
     )
 }
